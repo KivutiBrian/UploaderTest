29e8a7099971457afd1c85b4f2b1221a2cf73cb3
Gaideh <briankivuti1@gmail.com>
2021-12-30 14:03:01 +0300

    update 3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
 db = SQLAlchemy(app)
 
 
